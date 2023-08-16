017a905db1de309c92145c2de68e0d23060d9ebe
ImaiYuichi <ImaiYuichi>
2023-08-17 00:43:51 +0900

    GitHub Pagesで公開できるように設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://imaiyuichi.github.io/',
+    outDir: './docs',
+    base: '/portfolio02',
+});
