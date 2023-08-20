8b788ac853ee42b403d5420380bd7c7fa15ff149
ImaiYuichi <ImaiYuichi>
2023-08-20 13:32:05 +0900

    諸々修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://imaiyuichi.github.io/',
-    outDir: './docs',
-    base: '/docs',
-});
+export default defineConfig({});
