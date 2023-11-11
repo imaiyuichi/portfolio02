49c52e8d98b82ac151fe1cb8cc31c2f4463402f6
ImaiYuichi <ImaiYuichi>
2023-11-12 01:09:08 +0900

    環境など諸々調整

--- src/libs/macrocms.ts
+++ src/libs/macrocms.ts
@@ -1,28 +1,28 @@
-import { createClient, MicroCMSQueries } from 'microcms-js-sdk';
+import { createClient, MicroCMSQueries } from "microcms-js-sdk";
 
 const client = createClient({
-    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN, // ここに実際のサービスドメインを記述
-    apiKey: import.meta.env.MICROCMS_API_KEY, // ここに実際のAPIキーを記述
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN, // ここに実際のサービスドメインを記述
+  apiKey: import.meta.env.MICROCMS_API_KEY, // ここに実際のAPIキーを記述
 });
 
 //APIの呼び出し
 export const getTop = async (queries?: MicroCMSQueries) => {
-    return await client.get({ endpoint: 'top', queries });
+  return await client.get({ endpoint: "top", queries });
 };
 
 export const getBlogs = async (queries?: MicroCMSQueries) => {
-    return await client.get({ endpoint: 'blogs', queries });
+  return await client.get({ endpoint: "blogs", queries });
 };
 
 export const getBlogDetail = async (
-    blogId: string,
-    queries?: MicroCMSQueries
+  blogId: string,
+  queries?: MicroCMSQueries
 ) => {
-    return await client.getListDetail({
-        endpoint: 'blogs',
-        contentId: blogId,
-        queries,
-    });
+  return await client.getListDetail({
+    endpoint: "blogs",
+    contentId: blogId,
+    queries,
+  });
 };
 
 // //APIの呼び出し
--- src/script/utility/viewport-size.js
+++ src/script/utility/viewport-size.js
@@ -5,6 +5,6 @@ export const viewportSize = () => {
   const vw = window.innerWidth * 0.01;
   const vh = window.innerHeight * 0.01;
 
-  document.documentElement.style.setProperty("--vw", `${vw}px`);
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty('--vw', `${vw}px`);
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 };
