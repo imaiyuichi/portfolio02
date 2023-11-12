8d01b2b3fa9e1eb7e0462db956b0a4bd3b4fe2b4
ImaiYuichi <ImaiYuichi>
2023-11-12 10:55:36 +0900

    elslintの設定を追加

--- src/script/app.js
+++ src/script/app.js
@@ -1,31 +1,3 @@
-import { BREAKPOINT } from './variables';
-import { viewportSpFix } from './utility/viewport-fix';
-// import { viewportSize, viewportSizeSp } from './utility/viewport-size';
-
-// export const app = () => {
-//   window.addEventListener('load', () => {
-//     viewportSpFix();
-//     viewportSize();
-//   });
-
-//   window.addEventListener('resize', () => {
-//     viewportSpFix();
-//     viewportSize();
-//   });
-
-//   // 横のリサイズイベントのみを検知する
-//   let windowWidth = window.innerWidth;
-
-//   if (BREAKPOINT >= windowWidth) viewportSizeSp();
-
-//   window.addEventListener('resize', () => {
-//     if (BREAKPOINT >= windowWidth) {
-//       if (windowWidth === window.innerWidth) return;
-//       windowWidth = window.innerWidth;
-//       viewportSizeSp();
-//     }
-//   });
-// };
 export const app = () => {
   console.log('next-sinblog');
 };
